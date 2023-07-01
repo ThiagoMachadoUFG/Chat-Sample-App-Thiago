18b69443d7b4125cd8de2ddb3b384a6201f0aee8
ThiagoMachadoUFG <88293701+ThiagoMachadoUFG@users.noreply.github.com>
2023-07-01 19:45:14 -0300

    Update chatserver.py

--- chatserver.py
+++ chatserver.py
@@ -26,7 +26,57 @@ def handle_client(conn, addr):
             name=msg_pack[1]
             users[name]=addr[0]
         elif type=="msge":
-            print("msge")
+
+
+
+
+
+
+
+
+            msg = msg_pack[1]
+            dest = msg_pack[2]
+            src = msg_pack[3]
+            print("RELAYING MSG: " + msg + " - FROM: " + src + " - TO: " + dest)  # just print the message and destination
+
+            # Check that the destination exists
+            try:
+                dest_addr = users[dest]  # get address of destination in the registry
+            except:
+                conn.send(pickle.dumps("NACK"))  # to do: send a proper error code
+            else:
+                conn.send(pickle.dumps("ACK"))  # send ACK to client
+
+            conn.close()  # close the connection
+
+            # Forward the message to the recipient client
+            client_sock = socket(AF_INET, SOCK_STREAM)  # socket to connect to clients
+            dest_ip = dest_addr[0]
+            dest_port = 5002
+
+            try:
+                client_sock.connect((dest_ip, dest_port))
+            except:
+                print("Error: Destination client is down")
+            else:
+                msg_pack = (msg, src)
+                marshaled_msg_pack = pickle.dumps(msg_pack)
+                client_sock.send(marshaled_msg_pack)
+                marshaled_reply = client_sock.recv(1024)
+                reply = pickle.loads(marshaled_reply)
+                if reply != "ACK":
+                    print("Error: Destination client did not receive message properly")
+
+        client_sock.close()
+
+
+
+
+
+
+
+
+    
             
     except Exception as e:
         print("Error handling client:", e)
