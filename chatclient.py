8185d367a9ca02a0307b32e6f7bd832c5044486d
ThiagoMachadoUFG <88293701+ThiagoMachadoUFG@users.noreply.github.com>
2023-07-01 18:51:59 -0300

    Update chatclient.py

--- chatclient.py
+++ chatclient.py
@@ -30,8 +30,7 @@ try:
 except:
   print('Usage: python3 chatclient.py <Username>')
 client_sock = socket(AF_INET, SOCK_STREAM) # socket for server to connect to this client
-#my_ip = const.registry[me][0]   # If using a proper naming service, client should know its
-my_port = const.registry[me][1] # addresses (which it would register in the ns)
+my_port = 5002
 client_sock.bind(('0.0.0.0', my_port)) # NB: AWS instances don't allow binding to their public IP address!
 client_sock.listen(5)
 
@@ -39,6 +38,25 @@ client_sock.listen(5)
 # Put receiving thread to run
 recv_handler = RecvHandler(client_sock)
 recv_handler.start()
+
+server_sock = socket(AF_INET, SOCK_STREAM)
+try:
+    server_sock.connect((const.CHAT_SERVER_HOST, const.CHAT_SERVER_PORT))
+except:
+    print("Server is down. Exiting...")
+    exit(1)
+type="auth"
+msg_pack = (type, me)
+marshaled_msg_pack = pickle.dumps(msg_pack)
+server_sock.send(marshaled_msg_pack)
+marshaled_reply = server_sock.recv(1024)
+reply = pickle.loads(marshaled_reply)
+if reply != "ACK":
+    print("Error: Did not authenticate")
+else:
+    #print("Received Ack from server")
+    pass
+server_sock.close()
 
 #
 # Handle loop for user interaction (in the main thread)
